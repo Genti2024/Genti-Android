5fa39a201839dc1459c4787a24d363ed2377a247
Sangho Kim <marchbreeze03@gmail.com>
2024-05-06 01:23:35 +0900

    [ADD/#5] Login 배경 설정

--- presentation/src/main/java/kr/genti/presentation/auth/LoginActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/auth/LoginActivity.kt
@@ -1,9 +1,17 @@
 package kr.genti.presentation.auth
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityLoginBinding
 
 @AndroidEntryPoint
-class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login)
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
