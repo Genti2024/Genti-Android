93a04c3354b4bdd32b394c4a91f1e376a595854f
Sangho Kim <marchbreeze03@gmail.com>
2024-05-05 16:53:54 +0900

    [ADD/#5] LoginViewModel 추가

---
+++ presentation/src/main/java/kr/genti/presentation/auth/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package kr.genti.presentation.auth
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel
+    @Inject
+    constructor(
+        // private val authRepository: AuthRepository,
+    ) : ViewModel()
