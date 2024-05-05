0bad5e0087993224e576a0c76e7ed6821165d830
Sangho Kim <marchbreeze03@gmail.com>
2024-05-05 16:52:08 +0900

    [ADD/#5] LoginActivity 추가

---
+++ presentation/src/main/java/kr/genti/presentation/auth/LoginActivity.kt
@@ -0,0 +1,9 @@
+package kr.genti.presentation.auth
+
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityLoginBinding
+
+@AndroidEntryPoint
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login)
