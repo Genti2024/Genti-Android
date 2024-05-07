a0aa0f8a02f1721e4783d17101f1550c3a4de74f
Sangho Kim <marchbreeze03@gmail.com>
2024-05-08 03:54:06 +0900

    [FEAT/#11] 포즈선택뷰 Intent 설정

---
+++ presentation/src/main/java/kr/genti/presentation/select/pose/PoseViewModel.kt
@@ -0,0 +1,12 @@
+package kr.genti.presentation.select.pose
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PoseViewModel
+    @Inject
+    constructor(
+        // private val authRepository: AuthRepository,
+    ) : ViewModel()
