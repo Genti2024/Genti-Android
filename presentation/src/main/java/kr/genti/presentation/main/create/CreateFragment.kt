5085e00b580dd196149ce498d1eeb68845b1493a
Sangho Kim <marchbreeze03@gmail.com>
2024-05-08 03:18:24 +0900

    [FEAT/#9] 스크립트 입력 시 버튼 활성화

--- presentation/src/main/java/kr/genti/presentation/main/create/CreateFragment.kt
+++ presentation/src/main/java/kr/genti/presentation/main/create/CreateFragment.kt
@@ -2,6 +2,7 @@ package kr.genti.presentation.main.create
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseFragment
 import kr.genti.presentation.R
@@ -9,10 +10,18 @@ import kr.genti.presentation.databinding.FragmentCreateBinding
 
 @AndroidEntryPoint
 class CreateFragment() : BaseFragment<FragmentCreateBinding>(R.layout.fragment_create) {
+    private val viewModel by activityViewModels<CreateViewModel>()
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.vm = viewModel
     }
 }
