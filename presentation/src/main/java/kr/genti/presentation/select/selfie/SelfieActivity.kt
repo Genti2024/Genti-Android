209c551b06177599ef6b89ba755c93160a512a87
Sangho Kim <marchbreeze03@gmail.com>
2024-05-09 03:16:30 +0900

    [UI/#13] 사진뷰 불렛포인트 적용

--- presentation/src/main/java/kr/genti/presentation/select/selfie/SelfieActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/select/selfie/SelfieActivity.kt
@@ -3,11 +3,16 @@ package kr.genti.presentation.select.selfie
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.BulletSpan
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.core.extension.setStatusBarColorFromResource
+import kr.genti.core.extension.stringOf
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivitySelfieBinding
 
@@ -23,6 +28,7 @@ class SelfieActivity : BaseActivity<ActivitySelfieBinding>(R.layout.activity_sel
         initExitBtnListener()
         initNextBtnListener()
         setStatusBarColor()
+        setBulletPointList()
     }
 
     private fun initView() {
@@ -43,12 +49,38 @@ class SelfieActivity : BaseActivity<ActivitySelfieBinding>(R.layout.activity_sel
     }
 
     private fun initNextBtnListener() {
-        binding.btnPoseNext.setOnSingleClickListener {
-        }
+//        binding.btnPoseNext.setOnSingleClickListener {
+//        }
     }
 
     private fun setStatusBarColor() {
         setStatusBarColorFromResource(R.color.background_white)
+    }
+
+    private fun setBulletPointList() {
+        val points =
+            listOf(
+                stringOf(R.string.selfie_tv_guide_one),
+                stringOf(R.string.selfie_tv_guide_two),
+                stringOf(R.string.selfie_tv_guide_three),
+            )
+        val spannableStringBuilder = SpannableStringBuilder()
+        points.forEach { point ->
+            val spannableString =
+                SpannableString(point).apply {
+                    setSpan(
+                        BulletSpan(15),
+                        0,
+                        point.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                    )
+                }
+            with(spannableStringBuilder) {
+                append(spannableString)
+                append("\n")
+            }
+        }
+        binding.tvSelfieGuideBody.text = spannableStringBuilder
     }
 
     companion object {
