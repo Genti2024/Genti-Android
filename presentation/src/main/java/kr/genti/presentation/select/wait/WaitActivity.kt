9ae9dde6f0ee5b60cb8b5981111d7556b18ff7cc
Sangho Kim <marchbreeze03@gmail.com>
2024-05-09 19:05:22 +0900

    [UI/#15] 대기뷰 UI 구현

---
+++ presentation/src/main/java/kr/genti/presentation/select/wait/WaitActivity.kt
@@ -0,0 +1,34 @@
+package kr.genti.presentation.select.wait
+
+import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityWaitBinding
+
+@AndroidEntryPoint
+class WaitActivity : BaseActivity<ActivityWaitBinding>(R.layout.activity_wait) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initReturnBtnListener()
+        setStatusBarTransparent()
+    }
+
+    private fun initReturnBtnListener() {
+//        binding.btnPoseNext.setOnSingleClickListener {
+//        }
+    }
+
+    private fun setStatusBarTransparent() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            v.updatePadding(bottom = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom)
+            insets
+        }
+    }
+}
