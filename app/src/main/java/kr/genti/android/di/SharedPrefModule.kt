2754cb7282dbb5b5b737564a02b04a079ef790c4
Sangho Kim <marchbreeze03@gmail.com>
2024-05-12 22:19:29 +0900

    [ADD/#19] User SharedPreference 모듈 설정

---
+++ app/src/main/java/kr/genti/android/di/SharedPrefModule.kt
@@ -0,0 +1,26 @@
+package kr.genti.android.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kr.genti.data.local.UserSharedPref
+import kr.genti.data.local.UserSharedPrefImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPrefModule {
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        @ApplicationContext context: Context,
+    ): SharedPreferences = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun provideSharedPref(sharedPrefImpl: UserSharedPrefImpl): UserSharedPref = sharedPrefImpl
+}
