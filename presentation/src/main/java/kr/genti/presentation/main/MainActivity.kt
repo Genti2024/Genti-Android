1f6b281ca8de4e62b3ef1d43b4da2f65ba1610f0
Sangho Kim <marchbreeze03@gmail.com>
2024-05-27 02:08:45 +0900

    [FIX/#22] 바텀네비게이션 수정

--- presentation/src/main/java/kr/genti/presentation/main/MainActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/main/MainActivity.kt
@@ -1,13 +1,13 @@
 package kr.genti.presentation.main
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
-import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.core.extension.setStatusBarColorFromResource
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityMainBinding
@@ -29,13 +29,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private fun initBnvItemIconTintList() {
         with(binding.bnvMain) {
             itemIconTintList = null
-            selectedItemId = R.id.menu_create
+            selectedItemId = R.id.menu_feed
             labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_UNLABELED
         }
     }
 
     private fun initBnvItemSelectedListener() {
-        supportFragmentManager.findFragmentById(R.id.fcv_main) ?: navigateTo<CreateFragment>()
+        supportFragmentManager.findFragmentById(R.id.fcv_main) ?: navigateTo<FeedFragment>()
 
         binding.bnvMain.setOnItemSelectedListener { menu ->
             if (binding.bnvMain.selectedItemId == menu.itemId) {
@@ -50,16 +50,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
                 else -> return@setOnItemSelectedListener false
             }
+            binding.btnMenuCreate.isVisible = menu.itemId != R.id.menu_create
             true
         }
     }
 
-    fun initCreateBtnListener() {
-        binding.btnMenuCreate.setOnSingleClickListener {
+    private fun initCreateBtnListener() {
+        binding.btnMenuCreate.setOnClickListener {
             supportFragmentManager.commit {
                 replace<CreateFragment>(R.id.fcv_main)
             }
-            initBnvItemIconTintList()
+            binding.bnvMain.selectedItemId = R.id.menu_create
         }
     }
 
