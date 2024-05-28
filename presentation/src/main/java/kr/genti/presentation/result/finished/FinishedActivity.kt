3e9ca2ed45932d974ccb13a2e746490d73edfac0
Sangho Kim <marchbreeze03@gmail.com>
2024-05-29 00:19:28 +0900

    [ADD/#27] 생성 완료 액티비티 생성

---
+++ presentation/src/main/java/kr/genti/presentation/result/finished/FinishedActivity.kt
@@ -0,0 +1,14 @@
+package kr.genti.presentation.result.finished
+
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityFinishedBinding
+
+@AndroidEntryPoint
+class FinishedActivity : BaseActivity<ActivityFinishedBinding>(R.layout.activity_finished) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
