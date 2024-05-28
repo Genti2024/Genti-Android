bbbcacbf72f2429896b99b266c0dc120f286d47c
Sangho Kim <marchbreeze03@gmail.com>
2024-05-29 00:06:46 +0900

    [UI/#27] 글자 상단 강조점 설정

--- presentation/src/main/java/kr/genti/presentation/result/waiting/WaitingActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/result/waiting/WaitingActivity.kt
@@ -1,12 +1,17 @@
 package kr.genti.presentation.result.waiting
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.colorOf
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityWaitBinding
@@ -18,6 +23,7 @@ class WaitingActivity : BaseActivity<ActivityWaitBinding>(R.layout.activity_wait
 
         initReturnBtnListener()
         setStatusBarTransparent()
+        setEmphasizedText()
     }
 
     private fun initReturnBtnListener() {
@@ -31,6 +37,26 @@ class WaitingActivity : BaseActivity<ActivityWaitBinding>(R.layout.activity_wait
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             v.updatePadding(bottom = insets.getInsets(WindowInsetsCompat.Type.navigationBars()).bottom)
             insets
+        }
+    }
+
+    private fun setEmphasizedText() {
+        binding.tvWaitTitle.apply {
+            text =
+                SpannableStringBuilder(text).apply {
+                    setSpan(
+                        TextAppearanceSpan(context, R.style.TextAppearance_Genti_Headline1),
+                        7,
+                        10,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                    )
+                    setSpan(
+                        ForegroundColorSpan(colorOf(R.color.genti_green)),
+                        7,
+                        10,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                    )
+                }
         }
     }
 }
