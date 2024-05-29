daea3d9d8174ce0a009a839d29a5749523917234
Sangho Kim <marchbreeze03@gmail.com>
2024-05-30 04:27:54 +0900

    [ADD/#27] SpannableString으로 크기 조정

--- presentation/src/main/java/kr/genti/presentation/result/finished/FinishedActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/result/finished/FinishedActivity.kt
@@ -1,8 +1,13 @@
 package kr.genti.presentation.result.finished
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.colorOf
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityFinishedBinding
@@ -13,11 +18,46 @@ class FinishedActivity : BaseActivity<ActivityFinishedBinding>(R.layout.activity
         super.onCreate(savedInstanceState)
 
         initSaveBtnListener()
+        initReturnBtnListener()
+        initUnwantedBtnListener()
+        setEmphasizedText()
     }
 
     private fun initSaveBtnListener() {
         binding.btnDownload.setOnSingleClickListener {
             // TODO
+        }
+    }
+
+    private fun initReturnBtnListener() {
+        binding.btnReturnMain.setOnSingleClickListener {
+            // TODO
+        }
+    }
+
+    private fun initUnwantedBtnListener() {
+        binding.btnUnwanted.setOnSingleClickListener {
+            // TODO
+        }
+    }
+
+    private fun setEmphasizedText() {
+        binding.tvFinishedTitle.apply {
+            text =
+                SpannableStringBuilder(text).apply {
+                    setSpan(
+                        TextAppearanceSpan(context, R.style.TextAppearance_Genti_Headline1),
+                        0,
+                        11,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                    )
+                    setSpan(
+                        ForegroundColorSpan(colorOf(R.color.genti_green)),
+                        0,
+                        11,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
+                    )
+                }
         }
     }
 }
