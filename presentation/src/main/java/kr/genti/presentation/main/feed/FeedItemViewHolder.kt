d2225b087ba9cb239d3c240da43bfbe039ba5241
Sangho Kim <marchbreeze03@gmail.com>
2024-05-30 18:27:05 +0900

    [ADD/#32] 피드뷰 아이템 뷰홀더 설정

---
+++ presentation/src/main/java/kr/genti/presentation/main/feed/FeedItemViewHolder.kt
@@ -0,0 +1,21 @@
+package kr.genti.presentation.main.feed
+
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import kr.genti.domain.entity.response.FeedItemModel
+import kr.genti.presentation.databinding.ItemFeedItemBinding
+
+class FeedItemViewHolder(
+    val binding: ItemFeedItemBinding,
+) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun onBind(
+        item: FeedItemModel,
+        position: Int,
+    ) {
+        with(binding) {
+            ivFeedItemImage.setImageURI(item.url.toUri())
+            tvFeedItemDescription.text = item.prompt
+        }
+    }
+}
