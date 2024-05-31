aba2aab458428945fb7f1e5076a5649d6c7d65f5
Sangho Kim <marchbreeze03@gmail.com>
2024-06-01 02:13:21 +0900

    [ADD/#38] Create 서버통신 di 레이어 설정

--- app/src/main/java/kr/genti/android/di/RepositoryModule.kt
+++ app/src/main/java/kr/genti/android/di/RepositoryModule.kt
@@ -5,7 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kr.genti.data.repositoryImpl.AuthRepositoryImpl
+import kr.genti.data.repositoryImpl.CreateRepositoryImpl
 import kr.genti.domain.repository.AuthRepository
+import kr.genti.domain.repository.CreateRepository
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +16,8 @@ object RepositoryModule {
     @Provides
     @Singleton
     fun provideAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository = authRepositoryImpl
+
+    @Provides
+    @Singleton
+    fun provideCreateRepository(createRepositoryImpl: CreateRepositoryImpl): CreateRepository = createRepositoryImpl
 }
--- app/src/main/java/kr/genti/android/di/ServiceModule.kt
+++ app/src/main/java/kr/genti/android/di/ServiceModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kr.genti.data.service.AuthService
+import kr.genti.data.service.CreateService
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -16,4 +17,10 @@ object ServiceModule {
     fun provideAuthService(
         @RetrofitQualifier.JWT retrofit: Retrofit,
     ): AuthService = retrofit.create(AuthService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideCreateService(
+        @RetrofitQualifier.JWT retrofit: Retrofit,
+    ): CreateService = retrofit.create(CreateService::class.java)
 }
