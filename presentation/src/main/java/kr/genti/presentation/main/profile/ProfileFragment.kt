e59628429867a151643a0e68f8ab92c830f62bd4
Sangho Kim <marchbreeze03@gmail.com>
2024-05-31 15:48:08 +0900

    [FEAT/#34] 프로필뷰 어댑터 연동

--- presentation/src/main/java/kr/genti/presentation/main/profile/ProfileFragment.kt
+++ presentation/src/main/java/kr/genti/presentation/main/profile/ProfileFragment.kt
@@ -2,6 +2,8 @@ package kr.genti.presentation.main.profile
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseFragment
 import kr.genti.core.extension.initOnBackPressedListener
@@ -11,6 +13,13 @@ import kr.genti.presentation.databinding.FragmentProfileBinding
 
 @AndroidEntryPoint
 class ProfileFragment() : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    private var _adapter: ProfileAdapter? = null
+    val adapter
+        get() = requireNotNull(_adapter) { getString(R.string.adapter_not_initialized_error_msg) }
+
+    private val viewModel by activityViewModels<ProfileViewModel>()
+    var isWaiting = true
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
@@ -18,10 +27,46 @@ class ProfileFragment() : BaseFragment<FragmentProfileBinding>(R.layout.fragment
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        setAdapterList()
+        setMockImages()
+        observeStatus()
     }
 
     private fun initView() {
         initOnBackPressedListener(binding.root)
         setStatusBarColor(R.color.green_3)
+    }
+
+    private fun setAdapterList() {
+        _adapter =
+            ProfileAdapter(
+                imageClick = ::initImageBtnListner,
+            )
+    }
+
+    private fun initImageBtnListner(item: Int) {
+        // TODO
+    }
+
+    private fun setMockImages() {
+        adapter.addList(viewModel.mockItemList)
+    }
+
+    private fun observeStatus() {
+        // TODO: 서버통신으로 대체
+        with(binding) {
+            cvProfileNormal.isVisible = isWaiting != true
+            cvProfileWaiting.isVisible = isWaiting == true
+            if (!isWaiting) {
+                rvProfileNormalList.adapter = adapter
+            } else {
+                rvProfileWatingList.adapter = adapter
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _adapter = null
     }
 }
--- presentation/src/main/java/kr/genti/presentation/main/profile/ProfileViewModel.kt
+++ presentation/src/main/java/kr/genti/presentation/main/profile/ProfileViewModel.kt
@@ -2,6 +2,7 @@ package kr.genti.presentation.main.profile
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kr.genti.domain.entity.response.ImageModel
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +11,43 @@ class ProfileViewModel
     constructor(
         // private val profileRepository: ProfileRepository,
     ) : ViewModel() {
-        val mockItemList = listOf("")
+        val mockItemList =
+            listOf(
+                ImageModel(
+                    0,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/0eb2d7f2-90d2-436a-aa53-4ad7a414d805",
+                ),
+                ImageModel(
+                    0,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+                ImageModel(
+                    1,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+                ImageModel(
+                    2,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/0eb2d7f2-90d2-436a-aa53-4ad7a414d805",
+                ),
+                ImageModel(
+                    3,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/0eb2d7f2-90d2-436a-aa53-4ad7a414d805",
+                ),
+                ImageModel(
+                    4,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+                ImageModel(
+                    5,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+                ImageModel(
+                    6,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+                ImageModel(
+                    7,
+                    "https://github.com/Genti2024/Genti-Android/assets/97405341/68bf3348-f732-4874-947d-891f312b241e",
+                ),
+            )
     }
