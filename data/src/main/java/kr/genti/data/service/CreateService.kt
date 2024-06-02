c9e758a5827b0b81e8cb393fb075701942f98a03
Sangho Kim <marchbreeze03@gmail.com>
2024-06-03 00:04:38 +0900

    [FIX/#21] Post 통신 Body 수정

--- data/src/main/java/kr/genti/data/service/CreateService.kt
+++ data/src/main/java/kr/genti/data/service/CreateService.kt
@@ -4,6 +4,7 @@ import kr.genti.data.dto.BaseResponse
 import kr.genti.data.dto.request.S3RequestDto
 import kr.genti.data.dto.response.CreatePromptDto
 import kr.genti.data.dto.response.S3PresignedUrlDto
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -12,8 +13,12 @@ interface CreateService {
     suspend fun getExamplePrompts(): BaseResponse<List<CreatePromptDto>>
 
     @POST("/api/presigned-url")
-    suspend fun getSingleS3Url(request: S3RequestDto): BaseResponse<S3PresignedUrlDto>
+    suspend fun getSingleS3Url(
+        @Body request: S3RequestDto,
+    ): BaseResponse<S3PresignedUrlDto>
 
     @POST("/api/presigned-url/many")
-    suspend fun getMultiS3Url(request: List<S3RequestDto>): BaseResponse<List<S3PresignedUrlDto>>
+    suspend fun getMultiS3Url(
+        @Body request: List<S3RequestDto>,
+    ): BaseResponse<List<S3PresignedUrlDto>>
 }
