a5ba005f222456429f3d7a0ade2c93eb750e6317
Sangho Kim <marchbreeze03@gmail.com>
2024-06-04 22:40:53 +0900

    [FEAT/#48] Generate 서버통신 data 레이어 설정

--- data/src/main/java/kr/genti/data/dataSourceImpl/CreateDataSourceImpl.kt
+++ data/src/main/java/kr/genti/data/dataSourceImpl/CreateDataSourceImpl.kt
@@ -2,6 +2,7 @@ package kr.genti.data.dataSourceImpl
 
 import kr.genti.data.dataSource.CreateDataSource
 import kr.genti.data.dto.BaseResponse
+import kr.genti.data.dto.request.GenerateRequestDto
 import kr.genti.data.dto.request.S3RequestDto
 import kr.genti.data.dto.response.CreatePromptDto
 import kr.genti.data.dto.response.S3PresignedUrlDto
@@ -19,4 +20,6 @@ data class CreateDataSourceImpl
 
         override suspend fun getMultiS3Url(request: List<S3RequestDto>): BaseResponse<List<S3PresignedUrlDto>> =
             createService.getMultiS3Url(request)
+
+        override suspend fun postToGenerate(request: GenerateRequestDto): BaseResponse<Boolean> = createService.postToGenerate(request)
     }
