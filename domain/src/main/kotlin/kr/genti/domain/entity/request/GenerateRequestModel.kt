a8b1c1e15f2c0e60dc8b4d46f4521965b585c71d
Sangho Kim <marchbreeze03@gmail.com>
2024-06-04 22:23:07 +0900

    [FEAT/#48] Generate 서버통신 domain 레이어 설정

---
+++ domain/src/main/kotlin/kr/genti/domain/entity/request/GenerateRequestModel.kt
@@ -0,0 +1,12 @@
+package kr.genti.domain.entity.request
+
+import kr.genti.domain.enums.CameraAngle
+import kr.genti.domain.enums.ShotCoverage
+
+data class GenerateRequestModel(
+    val prompt: String,
+    val postPictureKey: String,
+    val facePictureKeyList: List<String>,
+    val cameraAngle: CameraAngle,
+    val shotCoverage: ShotCoverage,
+)
