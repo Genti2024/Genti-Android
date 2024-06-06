bd90260714dc47b59d8d4bd6b0e9b426c34155e9
Sangho Kim <marchbreeze03@gmail.com>
2024-06-06 19:31:59 +0900

    [ADD/#52] Finished뷰 뷰모델 설정

--- presentation/src/main/java/kr/genti/presentation/result/finished/FinishedActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/result/finished/FinishedActivity.kt
@@ -5,33 +5,39 @@ import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.text.style.TextAppearanceSpan
+import androidx.activity.viewModels
+import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
 import kr.genti.core.extension.colorOf
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityFinishedBinding
+import kr.genti.presentation.util.downloadImage
 
 @AndroidEntryPoint
 class FinishedActivity : BaseActivity<ActivityFinishedBinding>(R.layout.activity_finished) {
+    private val viewModel by viewModels<FinishedViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initSaveBtnListener()
         initReturnBtnListener()
         initUnwantedBtnListener()
+        setFinishedImage()
         setEmphasizedText()
     }
 
     private fun initSaveBtnListener() {
         binding.btnDownload.setOnSingleClickListener {
-            // TODO
+            downloadImage(viewModel.finishedImage.id, viewModel.finishedImage.url)
         }
     }
 
     private fun initReturnBtnListener() {
         binding.btnReturnMain.setOnSingleClickListener {
-            // TODO
+            finish()
         }
     }
 
@@ -39,6 +45,10 @@ class FinishedActivity : BaseActivity<ActivityFinishedBinding>(R.layout.activity
         binding.btnUnwanted.setOnSingleClickListener {
             // TODO
         }
+    }
+
+    private fun setFinishedImage() {
+        binding.ivFinishedImage.load(viewModel.finishedImage.url)
     }
 
     private fun setEmphasizedText() {
