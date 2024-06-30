667fab2f4fae4a2f232a3adb872659cd01530fb4
Sangho Kim <marchbreeze03@gmail.com>
2024-07-01 02:03:09 +0900

    [FIX/#59] enum class 수정사항 반영

--- domain/src/main/kotlin/kr/genti/domain/enums/CameraAngle.kt
+++ domain/src/main/kotlin/kr/genti/domain/enums/CameraAngle.kt
@@ -1,10 +1,10 @@
 package kr.genti.domain.enums
 
 enum class CameraAngle(private val description: String) {
-    NONE("앵글은 자유롭게 맡길래요"),
-    ABOVE("위에서 촬영"),
-    EYE_LEVEL("같은 높이에서 촬영"),
-    BELOW("아래에서 촬영"),
+    ANY("앵글은 자유롭게 맡길래요"),
+    HIGH("위에서 촬영"),
+    MIDDLE("눈높이에서 촬영"),
+    LOW("아래에서 촬영"),
     ;
 
     override fun toString(): String {
--- domain/src/main/kotlin/kr/genti/domain/enums/ShotCoverage.kt
+++ domain/src/main/kotlin/kr/genti/domain/enums/ShotCoverage.kt
@@ -1,10 +1,10 @@
 package kr.genti.domain.enums
 
 enum class ShotCoverage(private val description: String) {
-    NONE("프레임은 자유롭게 맡길래요"),
-    UPPER_BODY("허리 위로 촬영"),
-    KNEE_UP("무릎 위로 촬영"),
-    FULL_BODY("전신 촬영"),
+    ANY("프레임은 자유롭게 맡길래요"),
+    UPPER_BODY("바스트샷\n(상반신)"),
+    KNEE_SHOT("니샷\n(무릎 위)"),
+    FULL_BODY("풀샷\n(전신)"),
     ;
 
     override fun toString(): String {
