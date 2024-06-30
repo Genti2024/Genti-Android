68a0d614c763c11c557c3da55a4f95aa455d9bd6
Sangho Kim <marchbreeze03@gmail.com>
2024-07-01 04:31:05 +0900

    [FIX/#62] 피드뷰 서버통신 변동사항 대응

--- domain/src/main/kotlin/kr/genti/domain/entity/response/FeedItemModel.kt
+++ domain/src/main/kotlin/kr/genti/domain/entity/response/FeedItemModel.kt
@@ -1,5 +1,8 @@
 package kr.genti.domain.entity.response
 
+import kr.genti.domain.enums.PictureRatio
+import kr.genti.domain.enums.PictureType
+
 data class FeedItemModel(
     val picture: FeedPictureModel,
     val prompt: String,
@@ -8,5 +11,7 @@ data class FeedItemModel(
         val id: Long,
         val url: String,
         val key: String,
+        val pictureRatio: PictureRatio?,
+        val type: PictureType?,
     )
 }
