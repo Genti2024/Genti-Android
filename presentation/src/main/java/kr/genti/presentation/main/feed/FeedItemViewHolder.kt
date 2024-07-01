f9876f40d471e15758d8d5a5820e98395fd23aba
Sangho Kim <marchbreeze03@gmail.com>
2024-07-02 03:50:16 +0900

    [FIX/#43] 피드뷰 ratio에 맞춘 아이템 설정

--- presentation/src/main/java/kr/genti/presentation/main/feed/FeedItemViewHolder.kt
+++ presentation/src/main/java/kr/genti/presentation/main/feed/FeedItemViewHolder.kt
@@ -1,8 +1,10 @@
 package kr.genti.presentation.main.feed
 
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import kr.genti.domain.entity.response.FeedItemModel
+import kr.genti.domain.enums.PictureRatio
 import kr.genti.presentation.databinding.ItemFeedItemBinding
 
 class FeedItemViewHolder(
@@ -15,6 +17,16 @@ class FeedItemViewHolder(
         position: Int,
     ) {
         with(binding) {
+            tvFeedItemDescription.text = item.prompt
+
+            if (item.picture.pictureRatio?.name == PictureRatio.RATIO_3_2.name) {
+                (cvFeedItemImage.layoutParams as ConstraintLayout.LayoutParams).dimensionRatio =
+                    "3:2"
+            } else {
+                (cvFeedItemImage.layoutParams as ConstraintLayout.LayoutParams).dimensionRatio =
+                    "2:3"
+            }
+
             ivFeedItemImage.load(item.picture.url) {
                 listener(
                     onSuccess = { _, _ ->
@@ -22,7 +34,6 @@ class FeedItemViewHolder(
                     },
                 )
             }
-            tvFeedItemDescription.text = item.prompt
         }
     }
 }
