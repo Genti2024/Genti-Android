9dfe8760bf4f4c9605334842090368f931883cdd
Sangho Kim <marchbreeze03@gmail.com>
2024-07-04 05:30:32 +0900

    [ADD/#42] 이미지목록 서버통신 data 모듈 설정

--- data/src/main/java/kr/genti/data/dto/response/PicturePagedListDto.kt
+++ data/src/main/java/kr/genti/data/dto/response/PicturePagedListDto.kt
@@ -1,30 +1,68 @@
 package kr.genti.data.dto.response
 
+import kotlinx.serialization.Serializable
+import kr.genti.domain.entity.response.PicturePagedListModel
+
+@Serializable
 data class PicturePagedListDto(
     val totalPages: Int,
     val totalElements: Int,
     val size: Int,
     val content: List<ImageDto>,
     val number: Int,
-    val sort: PicturePageSortModel,
+    val sort: PicturePageSortDto,
     val numberOfElements: Int,
-    val pageable: PicturePageableModel,
+    val pageable: PicturePageableDto,
     val first: Boolean,
     val last: Boolean,
     val empty: Boolean,
 ) {
-    data class PicturePageSortModel(
+    @Serializable
+    data class PicturePageSortDto(
         val empty: Boolean,
         val sorted: Boolean,
         val unsorted: Boolean,
-    )
+    ) {
+        fun toModel() =
+            PicturePagedListModel.PicturePageSortModel(
+                empty,
+                sorted,
+                unsorted,
+            )
+    }
 
-    data class PicturePageableModel(
+    @Serializable
+    data class PicturePageableDto(
         val offset: Int,
-        val sort: PicturePageSortModel,
+        val sort: PicturePageSortDto,
         val paged: Boolean,
         val pageNumber: Int,
         val pageSize: Int,
         val unpaged: Boolean,
-    )
+    ) {
+        fun toModel() =
+            PicturePagedListModel.PicturePageableModel(
+                offset,
+                sort.toModel(),
+                paged,
+                pageNumber,
+                pageSize,
+                unpaged,
+            )
+    }
+
+    fun toModel() =
+        PicturePagedListModel(
+            totalPages,
+            totalElements,
+            size,
+            content.map { it.toModel() },
+            number,
+            sort.toModel(),
+            numberOfElements,
+            pageable.toModel(),
+            first,
+            last,
+            empty,
+        )
 }
--- data/src/main/java/kr/genti/data/service/GenerateService.kt
+++ data/src/main/java/kr/genti/data/service/GenerateService.kt
@@ -2,9 +2,19 @@ package kr.genti.data.service
 
 import kr.genti.data.dto.BaseResponse
 import kr.genti.data.dto.response.GenerateStatusDto
+import kr.genti.data.dto.response.PicturePagedListDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GenerateService {
     @GET("/api/users/picture-generate-requests/pending")
     suspend fun getGenerateStatus(): BaseResponse<GenerateStatusDto>
+
+    @GET("/api/users/pictures/my")
+    suspend fun getGeneratedPictureList(
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sortBy") sortBy: String?,
+        @Query("direction") direction: String?,
+    ): BaseResponse<PicturePagedListDto>
 }
