805f1bdda86ad5af96973cfb35d36d387386f956
Sangho Kim <marchbreeze03@gmail.com>
2024-07-30 22:08:55 +0900

    [ADD/#89] 회원가입 API di 모듈 설정

--- app/src/main/java/kr/genti/android/di/RepositoryModule.kt
+++ app/src/main/java/kr/genti/android/di/RepositoryModule.kt
@@ -8,11 +8,13 @@ import kr.genti.data.repositoryImpl.AuthRepositoryImpl
 import kr.genti.data.repositoryImpl.CreateRepositoryImpl
 import kr.genti.data.repositoryImpl.FeedRepositoryImpl
 import kr.genti.data.repositoryImpl.GenerateRepositoryImpl
+import kr.genti.data.repositoryImpl.InfoRepositoryImpl
 import kr.genti.data.repositoryImpl.UploadRepositoryImpl
 import kr.genti.domain.repository.AuthRepository
 import kr.genti.domain.repository.CreateRepository
 import kr.genti.domain.repository.FeedRepository
 import kr.genti.domain.repository.GenerateRepository
+import kr.genti.domain.repository.InfoRepository
 import kr.genti.domain.repository.UploadRepository
 import javax.inject.Singleton
 
@@ -22,6 +24,10 @@ object RepositoryModule {
     @Provides
     @Singleton
     fun provideAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository = authRepositoryImpl
+
+    @Provides
+    @Singleton
+    fun provideInfoRepository(infoRepositoryImpl: InfoRepositoryImpl): InfoRepository = infoRepositoryImpl
 
     @Provides
     @Singleton
