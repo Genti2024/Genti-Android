78371b63e92f0ba21bf7c0fa249868b642f604ba
Sangho Kim <marchbreeze03@gmail.com>
2024-08-04 03:15:22 +0900

    [CHORE/#101] version code 3 변경

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 2
+    const val versionCode = 3
     const val versionName = "1.0"
 }
