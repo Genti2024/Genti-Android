7ec82c7f0a4e0de22dc901672f2f07d01baa1c78
Sangho Kim <marchbreeze03@gmail.com>
2024-08-14 17:08:52 +0900

    [FEAT/#111] AmplitudeManager 구현

--- presentation/src/main/java/kr/genti/presentation/util/AmplitudeManager.kt
+++ presentation/src/main/java/kr/genti/presentation/util/AmplitudeManager.kt
@@ -3,6 +3,7 @@ package kr.genti.presentation.util
 import android.content.Context
 import com.amplitude.android.Amplitude
 import com.amplitude.android.Configuration
+import com.amplitude.android.events.Identify
 
 object AmplitudeManager {
     private lateinit var amplitude: Amplitude
@@ -20,14 +21,44 @@ object AmplitudeManager {
             )
     }
 
-    fun logEvent(
+    fun trackEvent(
         eventName: String,
-        properties: Map<String, Any> = emptyMap(),
+        properties1: Map<String, Any>? = null,
+        properties2: Map<String, Any>? = null,
     ) {
-        if (::amplitude.isInitialized) {
-            amplitude.logEvent(eventName, properties)
-        } else {
-            throw IllegalStateException("AmplitudeManager is not initialized. Call init() first.")
+        when {
+            properties1 == null && properties2 == null -> {
+                amplitude.track(eventName)
+            }
+
+            properties1 != null && properties2 == null -> {
+                amplitude.track(eventName, properties1)
+            }
+
+            properties1 != null && properties2 != null -> {
+                val combinedProperties = mutableMapOf<String, Any?>()
+                combinedProperties.putAll(properties1)
+                combinedProperties.putAll(properties2)
+                amplitude.track(eventName, combinedProperties)
+            }
         }
+    }
+
+    fun updateProperties(
+        propertyName: String,
+        values: String,
+    ) {
+        amplitude.identify(Identify().set(propertyName, values))
+    }
+
+    fun updateIntProperties(
+        propertyName: String,
+        intValues: Int,
+    ) {
+        amplitude.identify(Identify().set(propertyName, intValues))
+    }
+
+    fun plusIntProperties(propertyName: String) {
+        amplitude.identify(Identify().add(propertyName, 1))
     }
 }
