06cdc3b14bb69518928cd09471bffa9c5350fbc6
Sangho Kim <marchbreeze03@gmail.com>
2024-08-16 02:34:16 +0900

    [FIX/#111] 회원가입 정보 API data 모듈 수정

--- data/src/main/java/kr/genti/data/dataSource/InfoDataSource.kt
+++ data/src/main/java/kr/genti/data/dataSource/InfoDataSource.kt
@@ -2,9 +2,10 @@ package kr.genti.data.dataSource
 
 import kr.genti.data.dto.BaseResponse
 import kr.genti.data.dto.request.SignupRequestDto
+import kr.genti.data.dto.response.SignUpUserDto
 
 interface InfoDataSource {
-    suspend fun postSignupData(request: SignupRequestDto): BaseResponse<Boolean>
+    suspend fun postSignupData(request: SignupRequestDto): BaseResponse<SignUpUserDto>
 
     suspend fun postUserLogout(): BaseResponse<Boolean>
 
--- data/src/main/java/kr/genti/data/repositoryImpl/InfoRepositoryImpl.kt
+++ data/src/main/java/kr/genti/data/repositoryImpl/InfoRepositoryImpl.kt
@@ -3,6 +3,7 @@ package kr.genti.data.repositoryImpl
 import kr.genti.data.dataSource.InfoDataSource
 import kr.genti.data.dto.request.SignupRequestDto.Companion.toDto
 import kr.genti.domain.entity.request.SignupRequestModel
+import kr.genti.domain.entity.response.SignUpUserModel
 import kr.genti.domain.repository.InfoRepository
 import javax.inject.Inject
 
@@ -11,9 +12,9 @@ class InfoRepositoryImpl
     constructor(
         private val infoDataSource: InfoDataSource,
     ) : InfoRepository {
-        override suspend fun postSignupData(request: SignupRequestModel): Result<Boolean> =
+        override suspend fun postSignupData(request: SignupRequestModel): Result<SignUpUserModel> =
             runCatching {
-                infoDataSource.postSignupData(request.toDto()).response
+                infoDataSource.postSignupData(request.toDto()).response.toModel()
             }
 
         override suspend fun postUserLogout(): Result<Boolean> =
