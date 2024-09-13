5ecd447f635e9d33c02abf5932644920f5a1220f
Sangho Kim <marchbreeze03@gmail.com>
2024-09-14 03:41:30 +0900

    [ADD/#133] 오픈채팅뷰 manifest 등록

---
+++ presentation/src/main/java/kr/genti/presentation/result/openchat/OpenchatActivity.kt
@@ -0,0 +1,14 @@
+package kr.genti.presentation.result.openchat
+
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityOpenchatBinding
+
+@AndroidEntryPoint
+class OpenchatActivity : BaseActivity<ActivityOpenchatBinding>(R.layout.activity_openchat) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
