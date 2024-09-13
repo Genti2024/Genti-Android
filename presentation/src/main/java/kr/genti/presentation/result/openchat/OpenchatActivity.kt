67b3291499f65926a0ea37c2d7a4e37df0adbdab
Sangho Kim <marchbreeze03@gmail.com>
2024-09-14 04:35:38 +0900

    [FEAT/#133] 오픈채팅 API  구현

--- presentation/src/main/java/kr/genti/presentation/result/openchat/OpenchatActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/result/openchat/OpenchatActivity.kt
@@ -1,10 +1,20 @@
 package kr.genti.presentation.result.openchat
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kr.genti.core.base.BaseActivity
 import kr.genti.core.extension.setOnSingleClickListener
+import kr.genti.core.extension.stringOf
+import kr.genti.core.extension.toast
+import kr.genti.core.state.UiState
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityOpenchatBinding
 
@@ -17,8 +27,7 @@ class OpenchatActivity : BaseActivity<ActivityOpenchatBinding>(R.layout.activity
 
         initExitBtnListener()
         setTitleTextGradation()
-        setGuideTextInfo()
-        observeAccessibleState()
+        observeGetOpenchatState()
     }
 
     private fun initExitBtnListener() {
@@ -30,11 +39,33 @@ class OpenchatActivity : BaseActivity<ActivityOpenchatBinding>(R.layout.activity
         // TODO
     }
 
-    private fun setGuideTextInfo() {
-        // TODO
+    private fun observeGetOpenchatState() {
+        viewModel.getOpenchatState.flowWithLifecycle(lifecycle).distinctUntilChanged()
+            .onEach { state ->
+                when (state) {
+                    is UiState.Success -> {
+                        with(state.data) {
+                            if (accessible) {
+                                // TODO
+                            }
+                            url?.let { initEnterBtnListener(it) }
+                            count?.let { setGuideTextInfo(it) }
+                        }
+                    }
+
+                    is UiState.Failure -> toast(stringOf(R.string.error_msg))
+                    else -> return@onEach
+                }
+            }.launchIn(lifecycleScope)
     }
 
-    private fun observeAccessibleState() {
+    private fun initEnterBtnListener(url: String) {
+        binding.btnEnterOpenchat.setOnSingleClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
+    }
+
+    private fun setGuideTextInfo(count: Int) {
         // TODO
     }
 }
--- presentation/src/main/java/kr/genti/presentation/result/openchat/OpenchatViewModel.kt
+++ presentation/src/main/java/kr/genti/presentation/result/openchat/OpenchatViewModel.kt
@@ -1,8 +1,15 @@
 package kr.genti.presentation.result.openchat
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kr.genti.core.state.UiState
+import kr.genti.domain.entity.response.OpenchatModel
 import kr.genti.domain.repository.GenerateRepository
+import kr.genti.domain.repository.UserRepository
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +17,25 @@ class OpenchatViewModel
     @Inject
     constructor(
         private val generateRepository: GenerateRepository,
-    ) : ViewModel()
+        private val userRepository: UserRepository,
+    ) : ViewModel() {
+        private val _getOpenchatState = MutableStateFlow<UiState<OpenchatModel>>(UiState.Empty)
+        val getOpenchatState: StateFlow<UiState<OpenchatModel>> = _getOpenchatState
+
+        init {
+            getOpenchatData()
+        }
+
+        private fun getOpenchatData() {
+            _getOpenchatState.value = UiState.Loading
+            viewModelScope.launch {
+                generateRepository.getOpenchatData()
+                    .onSuccess {
+                        // TODO accessible 저장
+                        _getOpenchatState.value = UiState.Success(it)
+                    }.onFailure {
+                        _getOpenchatState.value = UiState.Failure(it.message.toString())
+                    }
+            }
+        }
+    }
