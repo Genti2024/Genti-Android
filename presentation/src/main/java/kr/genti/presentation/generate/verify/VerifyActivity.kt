9cb95b3830557a219a80d964c3f82d8d7fdfd918
Sangho Kim <marchbreeze03@gmail.com>
2024-09-29 02:03:28 +0900

    [UI/#137] 본인인증뷰 상태바 색상 적용

--- presentation/src/main/java/kr/genti/presentation/generate/verify/VerifyActivity.kt
+++ presentation/src/main/java/kr/genti/presentation/generate/verify/VerifyActivity.kt
@@ -3,6 +3,8 @@ package kr.genti.presentation.generate.verify
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.setNavigationBarColorFromResource
+import kr.genti.core.extension.setStatusBarColorFromResource
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityVerifyBinding
 
@@ -10,5 +12,15 @@ import kr.genti.presentation.databinding.ActivityVerifyBinding
 class VerifyActivity : BaseActivity<ActivityVerifyBinding>(R.layout.activity_verify) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setStatusBarColorFromResource(R.color.verify_bg)
+        setNavigationBarColorFromResource(R.color.verify_bg)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        setStatusBarColorFromResource(R.color.white)
+        setNavigationBarColorFromResource(R.color.white)
     }
 }
