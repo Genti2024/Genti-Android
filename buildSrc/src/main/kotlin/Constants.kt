86e254b83da10b8ed3bce47366c69415e28a8354
Sangho Kim <marchbreeze03@gmail.com>
2024-09-30 21:56:51 +0900

    [CHORE/#139] version v1.1.5 변경

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 13
-    const val versionName = "1.1.4"
+    const val versionCode = 14
+    const val versionName = "1.1.5"
 }
