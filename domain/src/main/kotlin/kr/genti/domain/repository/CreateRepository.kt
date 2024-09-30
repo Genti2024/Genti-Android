830649826f82b8031dcef2fe0307480c28018e5d
Sangho Kim <marchbreeze03@gmail.com>
2024-09-30 16:39:20 +0900

    [ADD/#139] 인증 사진 전송 API domain 모듈 설정

--- domain/src/main/kotlin/kr/genti/domain/repository/CreateRepository.kt
+++ domain/src/main/kotlin/kr/genti/domain/repository/CreateRepository.kt
@@ -1,6 +1,7 @@
 package kr.genti.domain.repository
 
 import kr.genti.domain.entity.request.CreateRequestModel
+import kr.genti.domain.entity.request.KeyRequestModel
 import kr.genti.domain.entity.request.S3RequestModel
 import kr.genti.domain.entity.response.S3PresignedUrlModel
 
@@ -10,4 +11,6 @@ interface CreateRepository {
     suspend fun getS3MultiUrl(request: List<S3RequestModel>): Result<List<S3PresignedUrlModel>>
 
     suspend fun postToCreate(request: CreateRequestModel): Result<Boolean>
+
+    suspend fun postToVerify(request: KeyRequestModel): Result<Boolean>
 }
