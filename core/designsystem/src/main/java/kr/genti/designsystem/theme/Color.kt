a50e545610e7d6979e8cfd7e20952ca9c3b418c9
Sangho Kim <marchbreeze03@gmail.com>
2025-03-04 23:36:00 +0900

    [ADD/#203] 색상 추가

---
+++ core/designsystem/src/main/java/kr/genti/designsystem/theme/Color.kt
@@ -0,0 +1,34 @@
+package kr.genti.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand Color
+val GentiGreen = Color(0xFF49F155)
+val GentiGradationStart = Color(0xFF6CEE2A)
+val GentiGradationEnd = Color(0xFF1CF48B)
+
+// System Color
+val Black = Color(0xFF030F0F)
+val Gray = Color(0xFF192222)
+val GrayBtn = Color(0xFF0D2D2B)
+val Disabled = Color(0xFF495959)
+
+// Etc.
+val KakaoYellow = Color(0xFFFEE500)
+val Red = Color(0xFFF1532F)
+
+// White
+val White = Color(0xFFFFFFFF)
+val White80 = Color(0xCCFFFFFF)
+val White60 = Color(0x99FFFFFF)
+val White40 = Color(0x66FFFFFF)
+val White30 = Color(0x4DFFFFFF)
+val White20 = Color(0x33FFFFFF)
+val White10 = Color(0x1AFFFFFF)
+
+// Transparent
+val Transparent = Color(0x00000000)
+val Transparent70 = Color(0xB3000000)
+val Transparent60 = Color(0x99000000)
+val Transparent50 = Color(0x80000000)
+val Transparent30 = Color(0x4D000000)
