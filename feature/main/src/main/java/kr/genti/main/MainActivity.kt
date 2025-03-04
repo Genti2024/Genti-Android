b69e0b890a690bf8012365290689301fc39151ef
Sangho Kim <marchbreeze03@gmail.com>
2025-03-05 03:43:55 +0900

    [FEAT/#205] MainActivity SAA 등록

--- feature/main/src/main/java/kr/genti/main/MainActivity.kt
+++ feature/main/src/main/java/kr/genti/main/MainActivity.kt
@@ -1,2 +1,27 @@
 package kr.genti.main
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.designsystem.theme.GentiTheme
+import kr.genti.main.navigation.rememberMainNavigator
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            val navigator = rememberMainNavigator()
+
+            GentiTheme {
+                MainScreen(navigator)
+            }
+        }
+    }
+}
