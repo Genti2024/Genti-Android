e29cf21fd848413e8836ef4d259559953cb4ed63
Sangho Kim <marchbreeze03@gmail.com>
2025-03-05 00:29:27 +0900

    [ADD/#203] 로딩뷰 컴포넌트 설정

--- core/designsystem/src/main/java/kr/genti/designsystem/component/layout/GentiLoadingScreen.kt
+++ core/designsystem/src/main/java/kr/genti/designsystem/component/layout/GentiLoadingScreen.kt
@@ -1,2 +1,47 @@
 package kr.genti.designsystem.component.layout
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import kr.genti.common.extension.noRippleClickable
+import kr.genti.core.designsystem.R
+import kr.genti.designsystem.theme.Transparent50
+
+@Composable
+fun StempoLoadingScreen(
+    modifier: Modifier = Modifier
+) {
+    val lottieLoading by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.lottie_loading)
+    )
+
+    Box(
+        modifier = modifier,
+    ) {
+        LottieAnimation(
+            composition = lottieLoading,
+            iterations = LottieConstants.IterateForever,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Transparent50)
+                .padding(horizontal = 50.dp)
+                .noRippleClickable { }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StempoLoadingScreenPreview() {
+    StempoLoadingScreen()
+}
