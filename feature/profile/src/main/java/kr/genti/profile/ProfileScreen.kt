1da4546a492d8d523ab5278a7afa9b66b3d7a615
Sangho Kim <marchbreeze03@gmail.com>
2025-03-08 15:08:36 +0900

    [UI/#215] 프로필뷰 상단 구조 구현

--- feature/profile/src/main/java/kr/genti/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/kr/genti/profile/ProfileScreen.kt
@@ -1,22 +1,38 @@
 package kr.genti.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kr.genti.common.extension.noRippleClickable
 import kr.genti.common.xml.extension.toast
 import kr.genti.core.designsystem.R
+import kr.genti.designsystem.component.layout.GentiLoadingScreen
+import kr.genti.designsystem.theme.Black
 import kr.genti.designsystem.theme.GentiTheme
+import kr.genti.designsystem.theme.White40
 
 @Composable
 internal fun ProfileRoute(
@@ -45,24 +61,71 @@ internal fun ProfileRoute(
 
     ProfileScreen(
         paddingValues = paddingValues,
+        isLoading = feedState.isLoading
     )
 }
 
 @Composable
 private fun ProfileScreen(
     modifier: Modifier = Modifier,
-    paddingValues: PaddingValues = PaddingValues()
+    paddingValues: PaddingValues = PaddingValues(),
+    isLoading: Boolean = false,
+    onSettingBtnClick: () -> Unit = {}
 ) {
     Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(paddingValues),
+            .background(Black)
+            .padding(bottom = paddingValues.calculateBottomPadding()),
     ) {
-        Text("Profile")
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(top = paddingValues.calculateTopPadding()),
+        ) {
+            ProfileTopBar(
+                onSettingBtnClick = onSettingBtnClick
+            )
+        }
+
+        GentiLoadingScreen(
+            isLoading = isLoading,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ProfileTopBar(
+    modifier: Modifier = Modifier,
+    onSettingBtnClick: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.profile_tv_title),
+            style = GentiTheme.typography.title,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_setting),
+            contentDescription = null,
+            tint = White40,
+            modifier = Modifier
+                .size(48.dp)
+                .padding(12.dp)
+                .padding(end = 4.dp)
+                .noRippleClickable { onSettingBtnClick() },
+        )
+    }
+}
+
+@Preview()
 @Composable
 private fun ProfileScreenPreview() {
     GentiTheme {
