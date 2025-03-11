3c4b72081845d86a72da77e97d562bd3c206ac91
Sangho Kim <marchbreeze03@gmail.com>
2025-03-12 07:38:46 +0900

    [UI/#225] 프롬프트뷰 텍스트필드 구현

--- feature/generate/src/main/java/kr/genti/generate/PromptInputScreen.kt
+++ feature/generate/src/main/java/kr/genti/generate/PromptInputScreen.kt
@@ -1,15 +1,21 @@
 package kr.genti.generate
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,9 +23,14 @@ import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import kr.genti.core.designsystem.R
+import kr.genti.designsystem.component.button.GentiGradationIconButton
 import kr.genti.designsystem.component.item.GentiCheckedText
+import kr.genti.designsystem.theme.Black
 import kr.genti.designsystem.theme.GentiGreen
 import kr.genti.designsystem.theme.GentiTheme
+import kr.genti.designsystem.theme.Gray
+import kr.genti.designsystem.theme.Transparent
+import kr.genti.designsystem.theme.White40
 import kr.genti.domain.entity.response.PromptExampleModel
 import kr.genti.generate.component.PromptExamplePager
 
@@ -29,6 +40,8 @@ fun PromptInputScreen(
     isParentPic: Boolean = false,
     exampleList: ImmutableList<PromptExampleModel> = persistentListOf(),
     onExamplePageSwipe: () -> Unit = {},
+    prompt: String = "",
+    onPromptChanged: (String) -> Unit = {},
     onNextBtnClick: () -> Unit = {},
 ) {
     Box(
@@ -74,6 +87,53 @@ fun PromptInputScreen(
                 PromptExamplePager(
                     exampleList = exampleList,
                     onExamplePageSwipe = onExamplePageSwipe
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .background(
+                    color = Gray,
+                    shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+                )
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Black, shape = RoundedCornerShape(10.dp))
+                    .padding(8.dp),
+            ) {
+                BasicTextField(
+                    value = prompt,
+                    onValueChange = { newValue -> onPromptChanged(newValue) },
+                    textStyle = GentiTheme.typography.body2,
+                    cursorBrush = SolidColor(Transparent),
+                    decorationBox = { innerTextField ->
+                        Box {
+                            if (prompt.isEmpty()) {
+                                Text(
+                                    text = stringResource(id = R.string.create_et_script_hint),
+                                    style = GentiTheme.typography.body2,
+                                    color = White40
+                                )
+                            }
+                            innerTextField()
+                        }
+                    },
+                    modifier = Modifier
+                        .weight(1F)
+                        .align(Alignment.CenterVertically)
+                        .padding(8.dp)
+                )
+
+                GentiGradationIconButton(
+                    iconRes = R.drawable.ic_play,
+                    isActivate = prompt.isNotEmpty(),
+                    onBtnClick = onNextBtnClick
                 )
             }
         }
