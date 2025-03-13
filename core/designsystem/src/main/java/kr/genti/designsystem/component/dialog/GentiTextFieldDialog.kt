ed33ec478190904fb0b21e1db350fa7dac96d596
Sangho Kim <marchbreeze03@gmail.com>
2025-03-14 01:27:28 +0900

    [FIX/#233] 텍스트필드 다이얼로그 claerFocus 추가

--- core/designsystem/src/main/java/kr/genti/designsystem/component/dialog/GentiTextFieldDialog.kt
+++ core/designsystem/src/main/java/kr/genti/designsystem/component/dialog/GentiTextFieldDialog.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,12 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import kr.genti.common.extension.noRippleClickable
 import kr.genti.core.designsystem.R
 import kr.genti.designsystem.component.button.GentiButton
 import kr.genti.designsystem.theme.Black
@@ -57,13 +57,19 @@ fun GentiTextFieldDialog(
 ) {
     Dialog(
         onDismissRequest = onDismissRequest,
-        properties = DialogProperties(usePlatformDefaultWidth = false, decorFitsSystemWindows = false),
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false,
+            decorFitsSystemWindows = false
+        ),
     ) {
+        val focusManager = LocalFocusManager.current
+
         Box(
             modifier = modifier
                 .fillMaxSize()
                 .background(Transparent50)
                 .imePadding()
+                .noRippleClickable { focusManager.clearFocus() }
                 .padding(horizontal = 16.dp),
             contentAlignment = Alignment.Center
         ) {
