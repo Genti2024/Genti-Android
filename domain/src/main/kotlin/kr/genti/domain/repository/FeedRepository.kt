fdd9725b2589b2896e6ca33bc3730dd6b99e2e51
Sangho Kim <marchbreeze03@gmail.com>
2025-03-24 01:12:44 +0900

    [FEAT/#237] Feed 관련 통신 Usecase 변경

--- domain/src/main/kotlin/kr/genti/domain/repository/FeedRepository.kt
+++ domain/src/main/kotlin/kr/genti/domain/repository/FeedRepository.kt
@@ -3,5 +3,5 @@ package kr.genti.domain.repository
 import kr.genti.domain.entity.response.FeedItemModel
 
 interface FeedRepository {
-    suspend fun getExampleItems(): Result<List<FeedItemModel>>
+    suspend fun getExampleItems(): List<FeedItemModel>
 }
